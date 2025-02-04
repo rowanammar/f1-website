80c35157b3c07f16491f6d5a12f394edea2d8af2
Rawan <rawanehab523@gmail.com>
2025-02-04 13:49:37 +0200

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/f1-website/' : '/',
   plugins: [react()],
 });

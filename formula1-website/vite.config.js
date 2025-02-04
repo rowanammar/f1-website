3b2fe7e8b95cbb0e93232559eb4ac414d9c668c7
Rawan <rawanehab523@gmail.com>
2025-02-04 12:55:05 +0200

    Update vite.config.js

--- formula1-website/vite.config.js
+++ formula1-website/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/f1-website/',  // Set this to your repository name
   plugins: [react()],
 })
